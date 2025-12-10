2416a48c47a8b6f709947b83834343fec6425b20
Precious Idemudia <63691157+Idemudia-Precious@users.noreply.github.com>
2025-12-10 12:47:34 +0100

    Update project links and job titles in index.ts

--- data/index.ts
+++ data/index.ts
@@ -92,7 +92,7 @@ export const navItems = [
       des: "A real Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
       img: "/p3.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://presh-imaginify.vercel.app/",
+      link: "https://github.com/Idemudia-Precious/Presh_Imaginify",
     },
     {
       id: 4,
@@ -173,29 +173,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Frontend Engineer - Standbic IBTC",
+      desc: "Collaborated in the development of the Stanbic IBTC bank's travel management module.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Software Engineering Intern - MTN",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Mobile Engineer - Vista MFB",
+      desc: "Developed Vista mobile app for both iOS & Android platforms using React Native.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
+      title: "Software Developer - MTN Hq",
       desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Software Engineer - Freelance",
+      desc: "Delivered responsive and scalable web and mobile features for multiple clients, collaborating on UI/UX, performance optimization.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
